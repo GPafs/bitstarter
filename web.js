a25cfda0ef7545a5fd380fcc205ba75749408035
GPafs <gpafios@gmail.com>
2013-07-31 11:28:19 +0000

    Added read from file index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var buff = new Buffer(fs.readFileSync(index.html), "utf-8")
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(Buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
